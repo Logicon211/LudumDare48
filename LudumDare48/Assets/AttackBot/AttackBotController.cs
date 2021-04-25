71ca321fddbbe52ce27ecf2fe3b3c097fef15549
Mitchell <heavyiam@hotmail.com>
2021-04-25 16:07:04 -0300

    Bullet shader

--- LudumDare48/Assets/AttackBot/AttackBotController.cs
+++ LudumDare48/Assets/AttackBot/AttackBotController.cs
@@ -5,15 +5,14 @@ using UnityEngine;
 public class AttackBotController : MonoBehaviour, IDamageable<float>, IKillable
 {
     public float health = 100f;
-    public float speed = 1f;
-    public float attackRange = 5f;
+    public float attackRange = 50f;
 
     public string targetTag = "Player";
     public string attackScriptName;
-    public GameObject target;
 
     float currentHealth;
     float currentAttackCooldown = 0f;
+    float attackCooldown = 3f;
 
     Transform targetTransform;
     Animator animator;
@@ -27,36 +26,33 @@ public class AttackBotController : MonoBehaviour, IDamageable<float>, IKillable
         currentHealth = health;
         attack = GetComponent<IAttack<Animator, GameObject>>();
         animator = GetComponentInChildren<Animator>();
-        if (attack != null) currentAttackCooldown = attack.GetCooldown();
+        GameObject possibleTarget = GameObject.FindGameObjectWithTag(targetTag);
+        targetTransform = possibleTarget.GetComponent<Transform>();
     }
 
     void Update()
     {
-        // Search for player if target is null
-        if (target == null)
+        // Cooldown check
+        if(currentAttackCooldown <= 0)
         {
-            GameObject possibleTarget = GameObject.FindGameObjectWithTag(targetTag);
-            if (possibleTarget != null)
+            // Range check
+            if(Vector3.Distance(transform.position, targetTransform.position) >= attackRange)
             {
-                target = possibleTarget;
-                targetTransform = target.GetComponent<Transform>();
+                PerformAttack();
             }
         }
-        PerformAttack();
+    }
 
-        // update values
+    void FixedUpdate()
+    {
         if (currentAttackCooldown > 0f) currentAttackCooldown -= Time.deltaTime;
-
     }
 
     void PerformAttack()
     {
-        if (Vector3.Distance(transform.position, targetTransform.position) >= attackRange) return;
-        if (attack != null && currentAttackCooldown <= 0f)
-        {
-            attack.Attack(animator, target);
-            currentAttackCooldown = attack.GetCooldown();
-        }
+        // attack.Attack(animator, target);
+
+        currentAttackCooldown = attackCooldown;
     }
 
     public void Damage(float damage)
@@ -67,7 +63,7 @@ public class AttackBotController : MonoBehaviour, IDamageable<float>, IKillable
 
     public void Kill()
     {
-        animator.SetTrigger("kill");
+        animator.SetTrigger("Death");
         NavMovement nav = GetComponent<NavMovement>();
         Rigidbody rb = GetComponent<Rigidbody>();
         if (nav != null) nav.Stop();

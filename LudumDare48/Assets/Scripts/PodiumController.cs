1f279c1992be472727d28516058d58ba89faa307
Broderick Beaman <broderick.beaman@unb.ca>
2021-04-25 14:17:35 -0300

    Podium text faces player

--- LudumDare48/Assets/Scripts/PodiumController.cs
+++ LudumDare48/Assets/Scripts/PodiumController.cs
@@ -33,7 +33,7 @@ public class PodiumController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        TextFacePlayer(player.transform.position);
     }
 
     void InitializeFloatingText()

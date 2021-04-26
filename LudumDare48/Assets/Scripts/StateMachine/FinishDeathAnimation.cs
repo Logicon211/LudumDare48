f63cae057c14df03d73f60d11d81be5ac8b4be52
Logan Marks <logicon211@gmail.com>
2021-04-26 17:37:51 -0300

    The polish

--- LudumDare48/Assets/Scripts/StateMachine/FinishDeathAnimation.cs
+++ LudumDare48/Assets/Scripts/StateMachine/FinishDeathAnimation.cs
@@ -7,7 +7,13 @@ public class FinishDeathAnimation : StateMachineBehaviour
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         EnemyControllerBase controller = animator.gameObject.GetComponentInParent<EnemyControllerBase>();
-        if (controller != null) Destroy(controller.gameObject, stateInfo.length);
+        if (controller != null) {
+            BearAttack bearAttack = controller.gameObject.GetComponent<BearAttack>();
+            if(bearAttack != null) {
+                bearAttack.enabled = false;
+            }
+            Destroy(controller.gameObject, stateInfo.length);
+        }
         Destroy(animator.gameObject, stateInfo.length);
     }
 }
